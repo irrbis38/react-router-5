86ee2e9bd3a5dd38ce114d158b768703d6ef39cd
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-03-03 15:57:54 +0300

    feat: add Recipe component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { About } from "./pages/About";
 import { Contacts } from "./pages/Contacts";
 import { NotFound } from "./pages/NotFound";
 import { Category } from "./pages/Category";
+import { Recipe } from "./pages/Recipe";
 
 function App() {
     return (
@@ -19,6 +20,7 @@ function App() {
                         <Route path="/about" component={About} />
                         <Route path="/contacts" component={Contacts} />
                         <Route path="/category/:name" component={Category} />
+                        <Route path="/meal/:id" component={Recipe} />
                         <Route component={NotFound} />
                     </Switch>
                 </main>
