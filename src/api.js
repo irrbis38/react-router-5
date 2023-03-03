792c282b973cc7ddca344b8574b06a48bed79e5e
Vasily Ilyuhin <v.ilyuhin@gmail.com>
2023-03-03 14:33:24 +0300

    feat: add CategoryItem and CategoryList

--- src/api.js
+++ src/api.js
@@ -4,3 +4,13 @@ export const getMealById = async (mealId) => {
     const response = await fetch(API_URL + "lookup.php?i=" + mealId);
     return response.json();
 };
+
+export const getAllCategories = async () => {
+    const response = await fetch(API_URL + "categories.php");
+    return response.json();
+};
+
+export const getFilteredCategories = async (catName) => {
+    const response = await fetch(API_URL + "filter.php?c=" + catName);
+    return response.json();
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,24 @@
+import { useState, useEffect } from "react";
+import { getAllCategories } from "../api";
+import { Preloader } from "./../components/Preloader";
+import { CategoryList } from "../components/CategoryList";
+
 export const Home = () => {
-    return <div className="home">Home page</div>;
+    const [catalog, setCatalog] = useState([]);
+
+    useEffect(() => {
+        getAllCategories().then((data) => {
+            setCatalog(data.categories);
+        });
+    }, []);
+
+    return (
+        <div className="home">
+            {!catalog.length ? (
+                <Preloader />
+            ) : (
+                <CategoryList catalog={catalog} />
+            )}
+        </div>
+    );
 };
